9085738a6be76024ea40e15db8fd9d5d48a53190
Liana Simonian <liana.simoniian@gmail.com>
2025-09-06 19:54:58 +0300

    fist commit

---
+++ NZWalks/NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+
+    //https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET: https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "July", "Lia", "Sergey", "Ani ", " Olga", " Yan" };
+
+            return Ok(studentNames);
+
+        }
+    }
+}
