994cb20ff8dfc30c79b887d83ff3c09f96b18cb2
Liana Simonian <liana.simniian@gmail.com>
2025-09-16 19:45:07 +0300

    Get All Regions Action Method

--- NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
 using System.Linq.Expressions;
 
@@ -12,27 +13,16 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = new List<Region>
-            {
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Auckland region",
-                    Code = "AKL",
-                    RegionImageUrl = "https://media.istockphoto.com/id/691610536/photo/aucklands-skyline-at-dawn.jpg?s=1024x1024&w=is&k=20&c=7hoz9PnQjs4HGkFzDJL0AAPoY1a3duT3rbtwEaEPxXE="
-                },
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = " Wellington region",
-                    Code = "WLG",
-                    RegionImageUrl = " https://media.istockphoto.com/id/464378599/photo/wellington-bay-and-harbour.jpg?s=1024x1024&w=is&k=20&c=di_6JaJbuVdJeirBctyHO6QJJFzw2LM3kkCU6J5JTFU="
-                }
-
-            };
+            var regions = dbContext.Regions.ToList();
             return Ok(regions);
 
         }
